3d88bae5422a1d190c0b6e94f9a4633fc9088ded
Dhiraj Kumar <dheerprajapat84@gmail.com>
2025-03-18 22:26:23 +0530

    Added springboot server

---
+++ Server/src/main/java/com/neo/NeoBotAIServer/NeoBotAiServerApplication.java
@@ -0,0 +1,13 @@
+package com.neo.NeoBotAIServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoBotAiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoBotAiServerApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/neo/NeoBotAIServer/NeoBotAiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.NeoBotAIServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoBotAiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
